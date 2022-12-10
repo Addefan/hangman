8a0255c7d5836d4df5d9fa42737f8e80a0c015f3
lemmehoop <lemmehoop@gmail.com>
2022-12-10 13:14:48 +0300

    + getting games from server

--- ui/view/start_window.py
+++ ui/view/start_window.py
@@ -3,9 +3,10 @@ import socket
 import threading
 
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6 import QtWidgets, QtCore
 
-from ui.base.start_window import Ui_StartWindow # starting window in start_window.py
-from ui.base.game_window import Ui_GameWindow   # game window in game_window.py
+from ui.base.start_window import Ui_StartWindow  # starting window in start_window.py
+from ui.base.game_window import Ui_GameWindow  # game window in game_window.py
 from ui.view.game_window import GameWindow
 
 HOST = '127.0.0.1'
@@ -22,44 +23,22 @@ class StartWindow(QMainWindow, Ui_StartWindow):
 
         self.new_game.clicked.connect(self.open_game_window)
 
-        self.receive_thread = threading.Thread(target=self.receive)
-        self.receive_thread.start()
+        self.receive_games()
 
-    def receive(self):
-        is_games_received = False
+    def receive_games(self):
+        games_str = self.player.recv(2048).decode('ascii')
 
-        while True:
-            try:
-                if is_games_received:
-                    pass
-                else:
-                    games_str = self.player.recv(2048).decode('ascii')
-                    is_games_received = True
-                    games = json.loads(games_str)
-                    for game in games.keys():
-                        print(game)
-            except Exception as exc:
-                print(exc)
-                self.close()
+        for game in games_str.split():
+            self.withdraw_game(game)
 
-    # def withdraw_game(self):
-    #     self.game_label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
-    #     self.game_label.setGeometry(QtCore.QRect(10, 9, 621, 31))
-    #     self.game_label.setStyleSheet("background: rgb(221, 226, 255)")
-    #     self.game_label.setObjectName("game_label")
-    #     self.Connect = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-    #     self.Connect.setGeometry(QtCore.QRect(553, 15, 71, 20))
-    #     self.Connect.setStyleSheet("background: rgb(31, 44, 56);\n"
-    #                                "border-radius: 3;\n"
-    #                                "color: white;\n"
-    #                                "")
-    #     self.Connect.setObjectName("Connect")
-    #     self.scroll_games.setWidget(self.scrollAreaWidgetContents)
-    #     StartWindow.setCentralWidget(self.centralwidget)
-    #
-    #     _translate = QtCore.QCoreApplication.translate
-    #     self.game_label.setText(_translate("StartWindow", "Game label"))
-    #     self.Connect.setText(_translate("StartWindow", "CONNECT"))
+    def withdraw_game(self, game_name):
+        new_label = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+        new_label.setMaximumSize(QtCore.QSize(16777215, 25))
+        new_label.setObjectName(game_name)
+
+        _translate = QtCore.QCoreApplication.translate
+        new_label.setText(_translate("StartWindow", game_name))
+        self.verticalLayout.addWidget(new_label)
 
     def open_game_window(self):
         self.game = GameWindow()
